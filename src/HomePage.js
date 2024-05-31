43a3f2c958171dc45efd296d6c8ba5ce2c14268f
iovin2001 <100416507+iovin2001@users.noreply.github.com>
2024-05-31 18:19:12 +0200

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -432,7 +432,6 @@ const HomePage = () => {
                       <div className="div-30">Lotti Presentati</div>
                     </div>
                     <div className="div-31">
-                      <button onClick={() => handleBulkDelete('lotti')}>Elimina Selezionati</button>
                       <table className="aste-table">
                         <thead>
                           <tr>
@@ -442,6 +441,10 @@ const HomePage = () => {
                             <th>Categoria</th>
                             <th>Prezzo Iniziale</th>
                             <th>Azioni</th>
+                            <th>Azioni <button onClick={() => handleBulkDelete('asta')}>
+                                  <svg fill="#065a74" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#065a74" className="icon glyph"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M17,4V5H15V4H9V5H7V4A2,2,0,0,1,9,2h6A2,2,0,0,1,17,4Z"></path><path d="M20,6H4A1,1,0,0,0,4,8H5V20a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V8h1a1,1,0,0,0,0-2ZM11,17a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Zm4,0a1,1,0,0,1-2,0V11a1,1,0,0,1,2,0Z"></path></g></svg>
+                                </button>
+                            </th>
                           </tr>
                         </thead>
                         <tbody>
